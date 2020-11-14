a990ed636ffe948c3a67a35b5707c832487ff806
The-Verdugo <elvergpe@gmail.com>
2020-11-13 17:18:15 -0700

    bootstrap y template
    Se agrego bootstrap, sass y los templates para el proyecto

---
+++ resources/views/Index.blade.php
@@ -0,0 +1,5 @@
+@extends('template')
+@section('titulo', 'Doble Region')
+@section('body')
+
+@endsection
---
+++ resources/views/template.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <title>@yield('titulo')</title>
+</head>
+<body>
+    <header>
+        <div class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                  <div class="navbar-nav">
+                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Features</a>
+                    <a class="nav-item nav-link" href="#">Pricing</a>
+                    <a class="nav-item nav-link" href="#">Disabled</a>
+                  </div>
+                </div>
+              </nav>
+        </div>
+    </header>
+    @yield('body')
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
