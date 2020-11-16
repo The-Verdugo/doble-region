c131f5b39c7cc6714da49230f113258a37e3914f
The-Verdugo <elvergpe@gmail.com>
2020-11-16 12:51:32 -0700

    Estilos para carousel y background
    estilos para carousel y background de la primera seccion

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -1,65 +1,52 @@
 @extends('template')
 @section('titulo', 'Doble Region')
 @section('body')
-    <section class="fondofirtssection"></section>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+<div id="carouselIndex" class="carousel slide carousel-fade" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100" src="https://images3.alphacoders.com/151/151409.jpg">
+        <div class="carousel-caption d-none d-md-block">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="h2-responsive">Proximos Conciertos</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem minima esse sapiente, voluptatibus laboriosam rerum, saepe odio delectus molestias perferendis sint. Eos minima reprehenderit animi iure iste sint aliquid vero.</p>
+                        <a class="btn btn-outline-primary waves-effect" href="#">Ver mas</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(33).jpg">
+        <div class="carousel-caption d-none d-md-block">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="h2-responsive">Proximos Conciertos</h2>
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem minima esse sapiente, voluptatibus laboriosam rerum, saepe odio delectus molestias perferendis sint. Eos minima reprehenderit animi iure iste sint aliquid vero.</p>
+                        <a class="btn btn-outline-primary waves-effect" href="#">Ver mas</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg">
+      </div>
+    </div>
+</div>
+    <section class="fondofirtssection view">
+        <div class="mask rgba-stylish-light d-flex justify-content-center align-items-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 text-white">
+                        <h1 class="h1-responsive">Grupo Doble Region</h1>
+                        <hr class="bg-light">
+                        <p class="h4-responsive text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ipsam blanditiis, laudantium voluptates rem suscipit mollitia, quo sunt nulla quis pariatur deleniti expedita iste qui! Sit rerum aliquid nobis labore.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -8,10 +8,9 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <!-- Google Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
-    <!-- Bootstrap core CSS -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <!-- Material Design Bootstrap -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    {{-- Hojas de estilo --}}
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/estilos-index.css')}}">
     <link rel="stylesheet" href="{{asset('css/estilos-menu.css')}}">
@@ -19,22 +18,31 @@
 </head>
 <body>
     <header>       
-        <nav class="navbar navbar-expand-lg navbar-dark fixed-top gris-transparent scrolling-navbar">
-          <div class="container">
-                <a class="navbar-brand" href="{{Route('Inicio')}}">Doble Regi&oacute;n</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                  <div class="navbar-nav">
-                  <a class="nav-item nav-link active" href="{{Route('Inicio')}}">Inicio</a>
-                    <a class="nav-item nav-link" href="{{Route('Albumes')}}">&Aacute;lbumes</a>
-                    <a class="nav-item nav-link" href="{{Route('Contacto')}}">Contacto</a>
-                    <a class="nav-item nav-link" href="{{Route('Biografia')}}">Biograf&iacute;a</a>
-                  </div>
-                </div>
-          </div>
-        </nav>
+      <nav class="navbar fixed-top navbar-expand-lg navbar-dark gris-transparent scrolling-navbar">
+        <div class="container">
+          <a class="navbar-brand" href="#"><strong>Doble Regi&oacute;n</strong></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+              <a class="nav-link" href="{{route('Inicio')}}">Inicio <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('Albumes')}}">&Aacute;lbumes</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('Contacto')}}">Contacto</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{route('Biografia')}}">Biograf&iacute;a</a>
+              </li>
+            </ul>
+          </div>      
+        </div>
+      </nav>
     </header>
     @yield('body')
     <footer>
@@ -47,8 +55,6 @@
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <!-- Bootstrap tooltips -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
-    <!-- Bootstrap core JavaScript -->
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
     <!-- MDB core JavaScript -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
 </body>
